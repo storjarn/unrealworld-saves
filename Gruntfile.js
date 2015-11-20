797034a74359279d3606ea2172ba4f7a54cb2960
Christopher Maples <christopher.maples@twc-contractor.com>
2015-11-19 23:38:55 -0700

    unzip available now

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,21 @@
+var savesFile = 'saves.zip';
+
 module.exports = function(grunt) {
     grunt.initConfig({
         compress: {
             main: {
                 options: {
-                    archive: 'saves.zip'
+                    archive: savesFile
                 },
-                files: [
-                    {
-                        expand: true,
-                        src: [
-                            'ANCESTORS/*',
-                            'BEORN/*',
-                            'STORJARN/*'
-                        ],
-                        dest: './'
-                    }
-                ]
+                files: [{
+                    expand: true,
+                    src: [
+                        'ANCESTORS/*',
+                        'BEORN/*',
+                        'STORJARN/*'
+                    ],
+                    dest: './'
+                }]
             }
         }
     });
@@ -23,6 +23,22 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-compress');
 
     grunt.registerTask('save', ['compress']);
+
+    grunt.registerTask('unzip', function() {
+        var done = this.async();
+
+        var exec = require('child_process').exec,
+            child;
+
+        child = exec('tar -zxvf ' + savesFile, function(error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+            done();
+        });
+    });
 
     grunt.registerTask('default', ['save']);
 };
