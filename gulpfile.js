98061960aa13db41c60967b8196536b54e577c90
Christopher Maples <cmaples77@gmail.com>
2020-02-01 14:41:47 -0800

    troubleshooting the DAT file issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,84 @@
-const gulp = require('gulp');
-const zip = require('gulp-zip');
+const gulp = require('gulp');           // runtime framework
+const argv = require('yargs').argv;     // runtime util
+const zip = require('gulp-zip');        // zip util
+const readline = require('readline');   // prompt util
 
-var argv = require('yargs').argv;
+// -=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-=-=-=-=-= Deterministic Constants
 
-var savesFile = 'saves.zip';
+const savesFile = 'saves.zip';
 
-gulp.task('compress', () => {
-    return gulp.src([
-        '**/ANCESTORS/**',
-        '**/STORJARN/**',
-        '**/HERMES/**'
-    ]).pipe(zip(savesFile))
-        .pipe(gulp.dest('./'));
-});
+// -=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-=-=-=-=-= Formatting Constants
 
-/**
- * @param message - --message "initial commit"
- */
-gulp.task('commit', () => {
+const consoleDivider = "-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-=-=-=-=-=";
+const consolePrompt = " ? ";
 
-    if (!argv.message) {
-        throw new Error("You need to supply a commit message using `--message`!");
+// -=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-=-=-=-=-= Task Constants
+
+const choices = {
+    compress: () => {
+        return gulp.src([
+            '**/ANCESTORS/**',
+            '**/STORJARN/**',
+            '**/HERMES/**'
+        ]).pipe(zip(savesFile))
+            .pipe(gulp.dest('./'));
+    },
+    /**
+     * @param message - --message "initial commit"
+     */
+    commit: () => {
+
+        if (!argv.message) {
+            throw new Error("You need to supply a commit message using `--message`!");
+        }
+
+        let cmd = ['git add -A', 'git commit -m "' + argv.message + '"', 'git push'];
+
+        return runChildProcess(cmd);
+    },
+    load: () => {
+        var cmd = ['git pull'];
+
+        switch (process.platform) {
+            case 'linux':
+                cmd.push('unzip -o ' + savesFile);
+                break;
+            case 'win32':
+                break;
+            case 'darwin':
+                cmd.push('tar -zxvf ' + savesFile);
+                break;
+        }
+
+        return runChildProcess(cmd);
     }
+};
 
-    let cmd = ['git add -A', 'git commit -m "' + argv.message + '"', 'git push'];
+// -=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-=-=-=-=-= Task Setup
 
-    return runChildProcess(cmd);
-});
+const { compress, commit, load } = choices;
 
-gulp.task('save',
+registerTask("compress", compress, "Zip up all the character and ancestor save files");
+
+registerTask("commit", commit, "Git commit and push the zip file, i.e. gulp commit --message \"My commit message\"");
+
+registerTask("save",
     gulp.series(
         'compress',
         'commit'
-    )
+    ),
+    "Compresses and uploads the latest changes.  Runs `compress` then `commit`."
 );
 
-gulp.task('load', () => {
-    var cmd = ['git pull'];
+registerTask("load", load, "Pull down the latest zip from the repo and unzip");
 
-    switch (process.platform) {
-        case 'linux':
-            cmd.push('unzip -o ' + savesFile);
-            break;
-        case 'win32':
-            break;
-        case 'darwin':
-            cmd.push('tar -zxvf ' + savesFile);
-            break;
-    }
+// gulp.task('default', gulp.series('save'));
 
-    return runChildProcess(cmd);
-});
+registerTask("default", (done) => {
+    promptTask(choices, done);
+}, "Lists the tasks");
 
-gulp.task('default', gulp.series('save'));
+// -=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-=-=-=-=-=
 
 function runChildProcess(cmd) {
     cmd = cmd || [];
@@ -74,4 +100,50 @@ function runChildProcess(cmd) {
     } else {
         throw new Error('cmd is empty');
     }
+}
+
+function promptTask(choices, done) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    const nl = '\n';
+    const list = Object.keys(choices).map(taskName => {
+        let desc = '';
+        if (choices[taskName].description) {
+            desc = ` - ${choices[taskName].description}`;
+        }
+        return `   * ${taskName}${desc}`;
+    });
+
+    const prompt = `
+${consoleDivider}
+
+Choose a task:
+
+${list.join(nl)}
+
+${consoleDivider}
+
+${consolePrompt}`;
+
+    rl.question(prompt, (answer) => {
+        if (choices[answer]) {
+            (gulp.series(answer)());
+        }
+        done();
+        rl.close();
+
+    });
+}
+
+function registerTask(
+    taskName /* : string*/,
+    fn /* : Function */,
+    desc /* : string */
+) {
+    fn.description = desc;
+    choices[taskName] = fn;
+    gulp.task(taskName, fn);
 }
